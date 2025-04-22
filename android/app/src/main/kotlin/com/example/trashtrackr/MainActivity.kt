536ed0462628235b33a1088e5ec8bd4e35feab19
itsmeejoshiee <joshgorospe03@gmail.com>
2025-04-22 10:58:30 +0800

    Add: init repo & file strucutre

---
+++ android/app/src/main/kotlin/com/example/trashtrackr/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.trashtrackr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:trashtrackr/frontend/camera_module.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Placeholder());
+  }
+}
