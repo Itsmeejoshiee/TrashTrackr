c8f1cc8d166a76d12b724fe14774f7b3854917ab
joaquingalang <galangjoaquin@gmail.com>
2025-04-23 13:57:57 +0800

    Add: constants with colors & typography

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+// COLORS
+const Color kAppleGreen = Color(0xFF628307);
+const Color kLightGreen = Color(0xFFE6E6B0);
+const Color kEerieBlack = Color(0xFF1D1D1D);
+const Color kDarkTeal = Color(0xFF003333);
+const Color kTeal = Color(0xFF339999);
+const Color kRed = Color(0xFFEA4A57);
+
+// TEXT STYLES
+const TextStyle kDisplayLarge = TextStyle(
+  fontSize: 57,
+  fontWeight: FontWeight.normal,
+  letterSpacing: 0,
+  fontFamily: 'Urbanist',
+);
+
+const TextStyle kDisplayMedium = TextStyle(
+  fontSize: 45,
+  fontWeight: FontWeight.normal,
+  letterSpacing: 0,
+  fontFamily: 'Urbanist',
+);
+
+const TextStyle kDisplaySmall = TextStyle(
+  fontSize: 36,
+  fontWeight: FontWeight.normal,
+  letterSpacing: 0,
+  fontFamily: 'Urbanist',
+);
+
+const TextStyle kHeadlineLarge = TextStyle(
+  fontSize: 32,
+  fontWeight: FontWeight.normal,
+  letterSpacing: 0,
+  fontFamily: 'Urbanist',
+);
+
+const TextStyle kHeadlineMedium = TextStyle(
+  fontSize: 28,
+  fontWeight: FontWeight.normal,
+  letterSpacing: 0,
+  fontFamily: 'Urbanist',
+);
+
+const TextStyle kHeadlineSmall = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.normal,
+  letterSpacing: 0,
+  fontFamily: 'Urbanist',
+);
+
+const TextStyle kTitleLarge = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0,
+  fontFamily: 'Urbanist',
+);
+
+const TextStyle kTitleMedium = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.15,
+  fontFamily: 'Urbanist',
+);
+
+const TextStyle kTitleSmall = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.1,
+  fontFamily: 'Urbanist',
+);
+
+const TextStyle kLabelLarge = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.1,
+  fontFamily: 'Urbanist',
+);
+
+const TextStyle kLabelMedium = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.5,
+  fontFamily: 'Urbanist',
+);
+
+const TextStyle kLabelSmall = TextStyle(
+  fontSize: 11,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.5,
+  fontFamily: 'Urbanist',
+);
+
+const TextStyle kBodyLarge = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.normal,
+  letterSpacing: 0.15,
+  fontFamily: 'Urbanist',
+);
+
+const TextStyle kBodyMedium = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.normal,
+  letterSpacing: 0.25,
+  fontFamily: 'Urbanist',
+);
+
+const TextStyle kBodySmall = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.normal,
+  letterSpacing: 0.4,
+  fontFamily: 'Urbanist',
+);
