dd109a62d1df5524a0757ca130bba1a102d25af0
joaquingalang <galangjoaquin@gmail.com>
2025-04-24 10:35:46 +0800

    Update: primary color as background

--- lib/features/auth/frontend/splash_screen.dart
+++ lib/features/auth/frontend/splash_screen.dart
@@ -7,7 +7,7 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kAppleGreen,
+      backgroundColor: Theme.of(context).primaryColor,
       body: Stack(
         children: [
           Align(
