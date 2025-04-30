ff54a9d07d3004970c61033ba77a30c0a8d6e025
joaquingalang <galangjoaquin@gmail.com>
2025-04-30 09:42:51 +0800

    Update: constant light gray color

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -5,6 +5,7 @@ const Color kAvocado = Color(0xFF628307);
 const Color kAppleGreen = Color(0xFF819D39);
 const Color kLightGreen = Color(0xFFE6E6B0);
 const Color kEerieBlack = Color(0xFF1D1D1D);
+const Color kLightGray = Color(0xFFF6F6F6);
 const Color kDarkTeal = Color(0xFF003333);
 const Color kTeal = Color(0xFF339999);
 const Color kRed = Color(0xFFEA4A57);
