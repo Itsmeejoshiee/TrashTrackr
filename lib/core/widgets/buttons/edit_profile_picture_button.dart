96fb501964aca8cf2c6a3b8b5e0b638278384eab
joaquingalang <galangjoaquin@gmail.com>
2025-04-30 09:45:38 +0800

    Add: edit profile picture button

---
+++ lib/core/widgets/buttons/edit_profile_picture_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:trashtrackr/core/utils/constants.dart';
+
+class EditProfilePictureButton extends StatelessWidget {
+  const EditProfilePictureButton({
+    super.key,
+    this.image,
+    required this.onPressed,
+  });
+
+  final ImageProvider? image;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: CircleAvatar(
+        backgroundImage: image ?? AssetImage(
+          'assets/images/placeholder_profile.jpg',
+        ),
+        radius: 50,
+        child: Align(
+          alignment: Alignment.bottomRight,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black38,
+                  offset: Offset(0, 2),
+                  blurRadius: 2,
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(3.0),
+              child: Icon(Icons.edit, color: Colors.black, size: 20),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
