2fb3a303774e64b2904468efbda60e954f67b620
joaquingalang <galangjoaquin@gmail.com>
2025-05-02 09:14:42 +0800

    Update: kiwi green constant color

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 const Color kAvocado = Color(0xFF628307);
 const Color kAppleGreen = Color(0xFF819D39);
 const Color kLightGreen = Color(0xFFE6E6B0);
+const Color kLightKiwi = Color(0x267FDD53);
 const Color kEerieBlack = Color(0xFF1D1D1D);
 const Color kLightGray = Color(0xFFF6F6F6);
 const Color kDarkTeal = Color(0xFF003333);
