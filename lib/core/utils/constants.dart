79b6a2eb821489bc2812575ff2c0059a4d493e21
itsmeejoshiee <joshgorospe03@gmail.com>
2025-05-02 13:20:17 +0800

    Add: Package for Google Fonts (Urbanist & Poppins)

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 // COLORS
 const Color kAvocado = Color(0xFF628307);
@@ -11,107 +12,119 @@ const Color kTeal = Color(0xFF339999);
 const Color kRed = Color(0xFFEA4A57);
 
 // TEXT STYLES
-const TextStyle kDisplayLarge = TextStyle(
+
+TextStyle kUrbanistDisplayLarge = GoogleFonts.urbanist(
   fontSize: 57,
   fontWeight: FontWeight.normal,
   letterSpacing: 0,
-  fontFamily: 'Urbanist',
 );
 
-const TextStyle kDisplayMedium = TextStyle(
+TextStyle kUrbanistDisplayMedium = GoogleFonts.urbanist(
   fontSize: 45,
   fontWeight: FontWeight.normal,
   letterSpacing: 0,
-  fontFamily: 'Urbanist',
 );
 
-const TextStyle kDisplaySmall = TextStyle(
+TextStyle kUrbanistDisplaySmall = GoogleFonts.urbanist(
   fontSize: 36,
   fontWeight: FontWeight.normal,
   letterSpacing: 0,
-  fontFamily: 'Urbanist',
 );
 
-const TextStyle kHeadlineLarge = TextStyle(
+TextStyle kUrbanistHeadlineLarge = GoogleFonts.urbanist(
   fontSize: 32,
   fontWeight: FontWeight.normal,
   letterSpacing: 0,
-  fontFamily: 'Urbanist',
 );
 
-const TextStyle kHeadlineMedium = TextStyle(
+TextStyle kUrbanistHeadlineMedium = GoogleFonts.urbanist(
   fontSize: 28,
   fontWeight: FontWeight.normal,
   letterSpacing: 0,
-  fontFamily: 'Urbanist',
 );
 
-const TextStyle kHeadlineSmall = TextStyle(
+TextStyle kUrbanistHeadlineSmall = GoogleFonts.urbanist(
   fontSize: 24,
   fontWeight: FontWeight.normal,
   letterSpacing: 0,
-  fontFamily: 'Urbanist',
 );
 
-const TextStyle kTitleLarge = TextStyle(
+TextStyle kUrbanistTitleLarge = GoogleFonts.urbanist(
   fontSize: 20,
   fontWeight: FontWeight.w500,
   letterSpacing: 0,
-  fontFamily: 'Urbanist',
 );
 
-const TextStyle kTitleMedium = TextStyle(
+TextStyle kUrbanistTitleMedium = GoogleFonts.urbanist(
   fontSize: 16,
   fontWeight: FontWeight.w500,
   letterSpacing: 0.15,
-  fontFamily: 'Urbanist',
 );
 
-const TextStyle kTitleSmall = TextStyle(
+TextStyle kUrbanistTitleSmall = GoogleFonts.urbanist(
   fontSize: 14,
   fontWeight: FontWeight.w500,
   letterSpacing: 0.1,
-  fontFamily: 'Urbanist',
 );
 
-const TextStyle kLabelLarge = TextStyle(
+TextStyle kUrbanistLabelLarge = GoogleFonts.urbanist(
   fontSize: 14,
   fontWeight: FontWeight.w500,
   letterSpacing: 0.1,
-  fontFamily: 'Urbanist',
 );
 
-const TextStyle kLabelMedium = TextStyle(
+TextStyle kUrbanistLabelMedium = GoogleFonts.urbanist(
   fontSize: 12,
   fontWeight: FontWeight.w500,
   letterSpacing: 0.5,
-  fontFamily: 'Urbanist',
 );
 
-const TextStyle kLabelSmall = TextStyle(
+TextStyle kUrbanistLabelSmall = GoogleFonts.urbanist(
   fontSize: 11,
   fontWeight: FontWeight.w500,
   letterSpacing: 0.5,
-  fontFamily: 'Urbanist',
 );
 
-const TextStyle kBodyLarge = TextStyle(
+TextStyle kUrbanistBodyLarge = GoogleFonts.urbanist(
   fontSize: 16,
   fontWeight: FontWeight.normal,
-  letterSpacing: 0.15,
-  fontFamily: 'Urbanist',
+  letterSpacing: 0.5,
 );
 
-const TextStyle kBodyMedium = TextStyle(
+TextStyle kUrbanistBodyMedium = GoogleFonts.urbanist(
   fontSize: 14,
   fontWeight: FontWeight.normal,
   letterSpacing: 0.25,
-  fontFamily: 'Urbanist',
 );
 
-const TextStyle kBodySmall = TextStyle(
+TextStyle kUrbanistBodySmall = GoogleFonts.urbanist(
   fontSize: 12,
   fontWeight: FontWeight.normal,
   letterSpacing: 0.4,
-  fontFamily: 'Urbanist',
+);
+
+// Poppins
+
+TextStyle kPoppinsBodyLarge = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.normal,
+  letterSpacing: 0,
+);
+
+TextStyle kPoppinsBodyMedium = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.normal,
+  letterSpacing: 0,
+);
+
+TextStyle kPoppinsDisplayRegular = GoogleFonts.poppins(
+  fontSize: 36,
+  fontWeight: FontWeight.normal,
+  letterSpacing: 0,
+);
+
+TextStyle kPoppinsDisplaySmall = GoogleFonts.poppins(
+  fontSize: 32,
+  fontWeight: FontWeight.normal,
+  letterSpacing: 0,
 );
