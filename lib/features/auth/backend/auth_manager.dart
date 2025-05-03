a08ee78cf28179c3b90e1a02ac4d336cf7d37f15
joaquingalang <galangjoaquin@gmail.com>
2025-05-03 20:09:42 +0800

    Add: auth manager

---
+++ lib/features/auth/backend/auth_manager.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:trashtrackr/core/utils/constants.dart';
+
+class AuthManager extends StatelessWidget {
+  const AuthManager({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
