9aa0b9b38546ac70abff99134b2429757b413119
joaquingalang <galangjoaquin@gmail.com>
2025-05-06 19:33:05 +0800

    Add: profile header

---
+++ lib/core/widgets/profile/profile_header.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:trashtrackr/core/utils/constants.dart';
+
+class ProfileHeader extends StatelessWidget {
+  const ProfileHeader({
+    super.key,
+    required this.username,
+    required this.image,
+    required this.followers,
+    required this.following,
+  });
+
+  final String username;
+  final ImageProvider image;
+  final int followers;
+  final int following;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.center,
+          child: Text(
+            username,
+            style: kHeadlineSmall.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        SizedBox(height: 15),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Wrap(
+              direction: Axis.vertical,
+              children: [
+                Text(
+                  '$followers',
+                  style: kTitleLarge.copyWith(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  'Followers',
+                  style: kLabelLarge.copyWith(
+                    color: kAvocado.withOpacity(0.3),
+                  ),
+                ),
+              ],
+            ),
+            CircleAvatar(
+              foregroundImage: image,
+              radius: 46,
+            ),
+            Wrap(
+              direction: Axis.vertical,
+              children: [
+                Text(
+                  '$following',
+                  style: kTitleLarge.copyWith(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  'Following',
+                  style: kLabelLarge.copyWith(
+                    color: kAvocado.withOpacity(0.3),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
