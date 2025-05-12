08da22589b5b03e1eda51ed0919c5574c340d021
joaquingalang <galangjoaquin@gmail.com>
2025-05-13 03:10:22 +0800

    Update: listens to auth stream

--- lib/features/auth/backend/auth_manager.dart
+++ lib/features/auth/backend/auth_manager.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:trashtrackr/core/services/auth_service.dart';
 import 'package:trashtrackr/core/utils/auth_state.dart';
-import 'package:trashtrackr/features/auth/backend/auth_bloc.dart';
 import 'package:trashtrackr/features/auth/frontend/welcome_screen.dart';
 import 'package:trashtrackr/features/dashboard/frontend/dashboard_screen.dart';
 
 class AuthManager extends StatelessWidget {
-  const AuthManager({super.key});
+  AuthManager({super.key});
+
+  final AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    final authViewModel = Provider.of<AuthBloc>(context);
-
-    switch (authViewModel.authState) {
-      case AuthState.welcome:
-        return WelcomeScreen();
-      case AuthState.authenticated:
-        return DashboardScreen();
-      case AuthState.unauthenticated:
-        return WelcomeScreen();
-    }
+    return StreamBuilder(
+      stream: _authService.authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return DashboardScreen();
+        } else {
+          return WelcomeScreen();
+        }
+      },
+    );
   }
 }
