4070e0a1e1f3f83daf0cf90c2dbe449759a2611b
itsmeejoshiee <joshgorospe03@gmail.com>
2025-05-12 18:35:33 +0800

    Add: Compression of Images

--- lib/features/post/backend/post_bloc.dart
+++ lib/features/post/backend/post_bloc.dart
@@ -1,20 +1,33 @@
-import 'package:flutter/material.dart';
+import 'dart:typed_data';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:image_picker/image_picker.dart';
 
 class PostBloc {
-  Future<XFile?> getImage() async {
+  Future<Uint8List?> getImage() async {
     final ImagePicker picker = ImagePicker();
     // Pick an image.
     final XFile? image = await picker.pickImage(source: ImageSource.gallery);
 
-    return image;
+    Uint8List? compressedImage = await compressImage(image!);
+    return compressedImage;
   }
 
-  Future<XFile?> getCameraImage() async {
+  Future<Uint8List?> getCameraImage() async {
     final ImagePicker picker = ImagePicker();
     // Capture an image
     final XFile? image = await picker.pickImage(source: ImageSource.camera);
 
-    return image;
+    Uint8List? compressedImage = await compressImage(image!);
+    return compressedImage;
+  }
+
+  Future<Uint8List?> compressImage(XFile image) async {
+    var result = await FlutterImageCompress.compressWithFile(
+      image.path,
+      quality: 50,
+      rotate: 0,
+      autoCorrectionAngle: true,
+    );
+    return result;
   }
 }
