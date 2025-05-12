9a6339c74522f3c3c4ef71c454a47d1abc647c31
bread-pit <tadiqueangelicamae@gmail.com>
2025-05-13 01:38:59 +0800

    Update: gemini service

--- lib/features/waste_scanner/backend/gemini_service.dart
+++ lib/features/waste_scanner/backend/gemini_service.dart
@@ -1,12 +1,9 @@
-import 'dart:convert';
 import 'dart:typed_data';
-import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:trashtrackr/core/models/scan_result_model.dart';
 
 class GeminiService {
-  // Initialize the Gemini model with the API key and safety settings.
   final geminiModel = GenerativeModel(
     model: 'gemini-2.0-flash',
     apiKey: dotenv.env['GEMINI_API_KEY']!,
@@ -16,21 +13,51 @@ class GeminiService {
     ],
   );
 
-  Future<String> classifyWaste(Uint8List imageBytes) async {
+  Future<ScanResult> classifyWaste(Uint8List imageBytes) async {
     try {
       final imagePart = DataPart('image/jpeg', imageBytes);
       final response = await geminiModel.generateContent([
         Content.multi([
           imagePart,
           TextPart(
-            'You are a waste classification expert. Analyze the object in this image and determine whether it is biodegradable or non-biodegradable. Provide a brief explanation for your classification.',
+            '''
+            You are a waste classification expert. Analyze the object in this image and return the following in clearly labeled format:
+            
+            Product Name: [e.g. Aluminum Soda Can]
+             
+            Material: [e.g. Plastic, Metal] Do not include other description.
+            - 
+            - 
+            - [increase number of bullets if necessary]
+              
+            Classification: [Biodegradable / Non-Biodegradable / Recyclable]  
+            
+            To Do: short to-do only, in 10 words
+            -  
+            -   
+            
+            Not To Do: short not to-do only, in 10 wordsS
+            -   
+            -   
+            
+            Pro Tip: [Short advice about proper disposal or recycling benefits.]
+            ''',
           ),
+
         ]),
       ]);
 
-      return response.text ?? 'No response from Gemini';
+      final text = response.text ?? 'No response from Gemini';
+      return ScanResult.fromResponse(text);
     } catch (e) {
-      return 'Error from Gemini: $e';
+      return ScanResult(
+        productName: 'Error',
+        materials: [],
+        classification: 'non-biodegradable',
+        toDo: [],
+        notToDo: [],
+        proTip: '',
+      );
     }
   }
 }
