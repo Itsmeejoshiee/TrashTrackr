c67bab694f18b1473d8c46c4a9e2edcb13e4aac7
joaquingalang <galangjoaquin@gmail.com>
2025-05-14 03:42:25 +0800

    Revert "Merge branch 'staging' into features/badges"
    This reverts commit a9c001c957094a8185e0733730c348c42d307711, reversing
    changes made to 911b502bc2f1303ff61607ce2d86ad8177954a72.

--- lib/features/badges/frontend/badge_screen.dart
+++ lib/features/badges/frontend/badge_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:trashtrackr/core/models/eco_badge.dart';
+import 'package:trashtrackr/core/utils/constants.dart';
+import 'package:trashtrackr/core/models/badge_model.dart';
+import 'package:trashtrackr/core/services/badge_service.dart';
 import 'package:trashtrackr/core/widgets/buttons/multi_action_fab.dart';
 import 'package:trashtrackr/core/widgets/bars/main_navigation_bar.dart';
 import 'package:trashtrackr/core/widgets/box/badge_box.dart';
@@ -12,27 +14,25 @@ class BadgeScreen extends StatefulWidget {
 }
 
 class _BadgeScreenState extends State<BadgeScreen> {
-  NavRoute _selectedRoute = NavRoute.badge;
 
-  void _selectRoute(NavRoute route) {
-    setState(() {
-      _selectedRoute = route;
-    });
-  }
+  final BadgeService _badgeService = BadgeService();
 
-  List<Widget> _badgeBoxBuilder() {
+  List<Widget> _badgeBoxBuilder(List<BadgeModel>? badges) {
     List<Widget> badgeBoxes = [];
-    for (int id = 1; id <= 14; id++) {
+    for (BadgeModel badge in badges!) {
       // TODO: Check if badge is earned by referencing user badges in database
-      final EcoBadge currentBadge = EcoBadge(id: id);
-      badgeBoxes.add(
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: BadgeBox(
-            badge: currentBadge,
-          ),
+      final BadgeModel currentBadge = BadgeModel(id: badge.id, percent: badge.percent);
+      final badgeBox = Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: BadgeBox(
+          badge: currentBadge,
         ),
       );
+      if (currentBadge.percent >= 1) {
+        badgeBoxes.insert(0, badgeBox);
+      } else {
+        badgeBoxes.add(badgeBox);
+      }
     }
     return badgeBoxes;
   }
@@ -44,25 +44,40 @@ class _BadgeScreenState extends State<BadgeScreen> {
     final double badgeTitleOffset =
         screenHeight / ((screenHeight < 800) ? 40 : 20);
     final double imageSize = (screenWidth / 2.5) + 60;
-    return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: badgeTitleOffset),
-          Image.asset(
-            'assets/images/badges/badges_title.png',
-            width: imageSize,
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: GridView.count(
-                crossAxisCount: 3,
-                children: _badgeBoxBuilder(),
+    return StreamBuilder(
+      stream: _badgeService.getBadgeStream(),
+      builder: (context, snapshot) {
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator(color: kAvocado,));
+        }
+
+        if (!snapshot.hasData || snapshot.data == null) {
+          return Center(child: Text('Badge data is not available.'));
+        }
+
+        final badges = snapshot.data;
+        return Scaffold(
+          body: Column(
+            children: [
+              SizedBox(height: badgeTitleOffset),
+              Image.asset(
+                'assets/images/badges/badges_title.png',
+                width: imageSize,
               ),
-            ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: GridView.count(
+                    crossAxisCount: 3,
+                    children: _badgeBoxBuilder(badges),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
--- lib/features/home/frontend/widgets/badge_carousel.dart
+++ lib/features/home/frontend/widgets/badge_carousel.dart
@@ -1,41 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:trashtrackr/core/models/eco_badge.dart';
+import 'package:trashtrackr/core/models/badge_model.dart';
+import 'package:trashtrackr/core/services/badge_service.dart';
+import 'package:trashtrackr/core/utils/constants.dart';
 import 'package:trashtrackr/core/widgets/box/badge_box.dart';
 
 
-class BadgeCarousel extends StatelessWidget {
-  const BadgeCarousel({super.key, required this.badgeIdList});
+class BadgeCarousel extends StatefulWidget {
+  const BadgeCarousel({super.key});
 
-  final List<int> badgeIdList;
+  @override
+  State<BadgeCarousel> createState() => _BadgeCarouselState();
+}
 
-  List<Widget> _badgeBoxBuilder() {
+class _BadgeCarouselState extends State<BadgeCarousel> {
+
+  final BadgeService _badgeService = BadgeService();
+
+  List<Widget> _badgeBoxBuilder(List<BadgeModel> badges) {
     List<Widget> badgeBoxes = [];
-    for (int id in badgeIdList) {
-      final EcoBadge currentBadge = EcoBadge(id: id);
-      final BadgeBox badgeBox = BadgeBox(
-        width: 112,
-        height: 112,
-        badge: currentBadge,
-      );
-      badgeBoxes.add(badgeBox);
-      badgeBoxes.add(SizedBox(width: 15));
+    for (BadgeModel badge in badges) {
+      if (badge.percent >= 1) {
+        final BadgeBox badgeBox = BadgeBox(
+          width: 112,
+          height: 112,
+          badge: badge,
+        );
+        badgeBoxes.add(badgeBox);
+        badgeBoxes.add(SizedBox(width: 15));
+      }
     }
     return badgeBoxes;
   }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 130,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: [
-          SizedBox(width: 32),
+    return StreamBuilder<List<BadgeModel>>(
+      stream: _badgeService.getBadgeStream(),
+      builder: (context, snapshot) {
 
-          ..._badgeBoxBuilder(),
-        ],
-      ),
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator(color: kAvocado,));
+        }
+
+        if (!snapshot.hasData || snapshot.data == null) {
+          return Center(child: Text('Badge data is not available.'));
+        }
+
+        final badges = snapshot.data;
+        return Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 130,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 32),
+
+                  ..._badgeBoxBuilder(badges!),
+                ],
+              ),
+            ),
+          ],
+        );
+      }
     );
   }
 }
