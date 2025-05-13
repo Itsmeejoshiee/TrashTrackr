b5c6b98fbc2dc066657a84637bb6f2ebed567dfb
joaquingalang <galangjoaquin@gmail.com>
2025-05-13 15:50:51 +0800

    Add: user provider

---
+++ lib/core/user_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:trashtrackr/core/models/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserProvider extends ChangeNotifier {
+
+  UserModel? _user;
+
+  UserModel? get user => _user;
+
+  void setUser(UserModel user) {
+    _user = user;
+    notifyListeners();
+  }
+
+  Future<void> loadUserFromFirestore(String uid) async {
+    DocumentSnapshot doc =
+        await FirebaseFirestore.instance.collection('users').doc(uid).get();
+
+    if (doc.exists) {
+      _user = UserModel.fromMap(doc.data() as Map<String, dynamic>);
+      notifyListeners();
+    }
+  }
+
+}
