f46447f3897eefeba99ba03191533345f9801fb2
joaquingalang <galangjoaquin@gmail.com>
2025-05-16 01:24:01 +0800

    Add: comment button

---
+++ lib/core/widgets/buttons/comment_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:trashtrackr/core/utils/constants.dart';
+
+class CommentButton extends StatelessWidget {
+  const CommentButton({
+    super.key,
+    this.isActive = false,
+    required this.onPressed,
+  });
+
+  final bool isActive;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Row(
+        spacing: 5,
+        children: [
+          Image.asset(
+            'assets/images/icons/comment_${isActive ? 'active' : 'inactive'}.png',
+            height: 19,
+          ),
+          (!isActive) ? Text(
+            'Comment  ',
+            style:
+            (!isActive)
+                ? kPoppinsBodySmall
+                : kPoppinsBodySmall.copyWith(
+              color: kAvocado,
+              fontWeight: FontWeight.bold,
+            ),
+          ) : SizedBox(),
+        ],
+      ),
+    );
+  }
+}
