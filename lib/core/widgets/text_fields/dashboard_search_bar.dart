7f0b13f4bc9da02a4fb181e5abf9814e5ccf832e
Joshie <120581896+Itsmeejoshiee@users.noreply.github.com>
2025-05-16 15:05:49 +0800

    Revert "Fix: merge conflict"

--- lib/core/widgets/text_fields/dashboard_search_bar.dart
+++ lib/core/widgets/text_fields/dashboard_search_bar.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:trashtrackr/core/utils/constants.dart';
+import 'package:trashtrackr/features/feed/frontend/widgets/filter_menu.dart';
 
-class DashboardSearchBar extends StatelessWidget {
+class DashboardSearchBar extends StatefulWidget {
   const DashboardSearchBar({
     super.key,
     required this.controller,
@@ -11,7 +12,6 @@ class DashboardSearchBar extends StatelessWidget {
     this.margin,
     required this.onSearch,
     this.onFilterTap,
-    this.onChanged,
     this.onSubmit,
   });
 
@@ -22,65 +22,91 @@ class DashboardSearchBar extends StatelessWidget {
   final EdgeInsets? margin;
   final VoidCallback onSearch;
   final VoidCallback? onFilterTap;
-  final ValueChanged<String>? onChanged;
   final ValueChanged<String>? onSubmit;
 
   @override
+  State<DashboardSearchBar> createState() => _DashboardSearchBarState();
+}
+
+class _DashboardSearchBarState extends State<DashboardSearchBar>
+    with TickerProviderStateMixin {
+  bool isFilterVisible = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      margin: margin ?? EdgeInsets.symmetric(vertical: 15),
-      decoration: ShapeDecoration(
-        gradient: LinearGradient(
-          begin: Alignment(0.00, 0.00),
-          end: Alignment(1.00, 1.00),
-          colors: [Colors.white, const Color(0xFFEAEAEA)],
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          margin: widget.margin ?? const EdgeInsets.symmetric(vertical: 15),
+          decoration: ShapeDecoration(
+            gradient: const LinearGradient(
+              begin: Alignment(0.00, 0.00),
+              end: Alignment(1.00, 1.00),
+              colors: [Colors.white, Color(0xFFEAEAEA)],
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            shadows: const [
+              BoxShadow(
+                color: Color(0xE5DDDDDD),
+                blurRadius: 13,
+                offset: Offset(5, 5),
+                spreadRadius: 0,
+              ),
+              BoxShadow(
+                color: Color(0x33DDDDDD),
+                blurRadius: 10,
+                offset: Offset(-5, 5),
+                spreadRadius: 0,
+              ),
+            ],
+          ),
+          child: TextField(
+            controller: widget.controller,
+            onSubmitted: widget.onSubmit,
+            style: kTitleMedium.copyWith(
+              color: const Color(0xFF616468),
+              fontWeight: FontWeight.bold,
+            ),
+            cursorColor: const Color(0xFF616468),
+            cursorHeight: 20,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              prefixIcon: IconButton(
+                icon: const Icon(
+                  Icons.search,
+                  color: Color(0xFF616468),
+                  size: 23,
+                ),
+                onPressed: widget.onSearch,
+              ),
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.tune_outlined, color: Color(0xFF616468)),
+                onPressed: () {
+                  setState(() {
+                    isFilterVisible = !isFilterVisible;
+                  });
+                },
+              ),
+              contentPadding: const EdgeInsets.only(top: 11),
+              hintText: widget.hintText,
+              hintStyle: kTitleMedium.copyWith(
+                color: const Color(0xFF616468),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            obscureText: widget.obscureText,
+          ),
         ),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        shadows: [
-          BoxShadow(
-            color: Color(0xE5DDDDDD),
-            blurRadius: 13,
-            offset: Offset(5, 5),
-            spreadRadius: 0,
-          ),
-          BoxShadow(
-            color: Color(0x33DDDDDD),
-            blurRadius: 10,
-            offset: Offset(-5, 5),
-            spreadRadius: 0,
-          ),
-        ],
-      ),
-      child: TextField(
-        controller: controller,
-        onChanged: onChanged,
-        onSubmitted: onSubmit,
-        style: kTitleMedium.copyWith(
-          color: Color(0xFF616468),
-          fontWeight: FontWeight.bold,
+        AnimatedSize(
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          alignment: Alignment.topCenter,
+          child: isFilterVisible ? const FilterMenu() : const SizedBox.shrink(),
         ),
-        cursorColor: Color(0xFF616468),
-        cursorHeight: 20,
-        decoration: InputDecoration(
-          border: InputBorder.none,
-          prefixIcon: IconButton(
-            icon: Icon(Icons.search, color: Color(0xFF616468), size: 23),
-            onPressed: onSearch,
-          ),
-          suffixIcon: IconButton(
-            onPressed: onFilterTap,
-            icon: Icon(Icons.tune_outlined, color: Color(0xFF616468)),
-          ),
-          contentPadding: EdgeInsets.only(top: 11),
-          hintText: hintText,
-          hintStyle: kTitleMedium.copyWith(
-            color: Color(0xFF616468),
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        obscureText: obscureText,
-      ),
+      ],
     );
   }
 }
